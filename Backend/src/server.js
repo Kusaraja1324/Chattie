5e50f7163aed7cb2cb88157aaaf2b84ab553b581
Kusaraja1324 <kusarajanaidana@gmail.com>
2026-03-13 19:54:10 +0530

    ready for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const dotenv = require('dotenv');
 const  router  = require('./Routers/auth_route.js');
 const messageRoutes = require("./Routers/message_route.js");
+const path = require('path');
+const fs = require('fs');
+
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 
@@ -10,6 +13,16 @@ const app = express();
 app.use(express.json());
 app.use("/api/auth", router);
 app.use("/api/message",messageRoutes);
+
+const frontendDistPath = path.join(__dirname, "../../Frontend/ChatBot/dist");
+
+if (fs.existsSync(frontendDistPath)) {
+  app.use(express.static(frontendDistPath))
+
+  app.get("/{*splat}", (_req,res)=>{
+    res.sendFile(path.join(frontendDistPath, "index.html"));
+  })
+}
 
 app.listen(PORT, () => {
   console.log("Server is running on port: "+ PORT);
